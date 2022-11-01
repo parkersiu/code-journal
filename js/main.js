eb7e4b5b1e221e8314e968bafff4db3adcbafccd
Parker Siu <parker.siu@gmail.com>
2022-11-01 23:32:46 +0000

    Fixed placeholder image and changed notes input to text area.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 var $photoURL = document.getElementById('url');
 
-var $icon = document.getElementById('icon');
-
 var $image = document.getElementById('image');
 
 var $form = document.getElementById('form');
@@ -9,8 +7,6 @@ var $form = document.getElementById('form');
 $photoURL.addEventListener('input', function (event) {
   var $url = $form.elements.url.value;
   $image.setAttribute('src', $url);
-  $image.className = '';
-  $icon.className = 'fa-regular fa-image hidden';
 });
 
 $form.addEventListener('submit', function (event) {
