52558db79326b6bf0d82bb35aac5cf44215bd933
Parker Siu <parker.siu@gmail.com>
2022-11-03 16:30:56 +0000

    Updated render function and event listeners.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var $new = document.getElementById('new');
 var $entries = document.getElementById('entries');
 var $navEntries = document.getElementById('nav-entries');
 var $entriesForm = document.getElementById('entry-form');
+var $ul = document.getElementById('entrylist');
 
 $photoURL.addEventListener('input', function (event) {
   var $url = $form.elements.url.value;
@@ -19,11 +20,11 @@ $form.addEventListener('submit', function (event) {
   values.notes = $form.elements.notes.value;
   values.nextEntryId = data.nextEntryId;
   data.nextEntryId++;
-  data.entries.push(values);
+  data.entries.unshift(values);
   $image.setAttribute('src', '');
+  $ul.prepend(renderEntries(values));
   $form.reset();
-  $entries.className = '';
-  $form.className = 'hidden';
+  updateView('entries');
 });
 
 function renderEntries(entry) {
@@ -40,26 +41,25 @@ function renderEntries(entry) {
   h2.textContent = entry.title;
   var p = document.createElement('p');
   p.textContent = entry.notes;
-  var $ul = document.getElementById('entrylist');
-  $ul.appendChild(li);
   li.appendChild(divRow);
   divRow.appendChild(divColumnHalf);
   divColumnHalf.appendChild(img);
   divRow.appendChild(divColumnHalf2);
   divColumnHalf2.appendChild(h2);
   divColumnHalf2.appendChild(p);
-  return $ul;
+  return li;
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
   for (var i = 0; i < data.entries.length; i++) {
-    renderEntries(data.entries[i]);
+    $ul.append(renderEntries(data.entries[i]));
   }
   updateView(data.view);
 });
 
 $new.addEventListener('click', function (event) {
   updateView('entry-form');
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
 
 $navEntries.addEventListener('click', function (event) {
